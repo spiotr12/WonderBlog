81bbdcdb98ff4712e0e8ea5f5af57bbf7a8eb5b8
Piotrek <starzec.piotr.12@gmail.com>
2015-11-05 21:20:59 +0000

    Directory set

--- site/index.php
+++ site/index.php
@@ -5,13 +5,6 @@
     <title></title>
 </head>
 <body>
-<h1>Hello World in public_html?</h1>
-<?php
-$space = "<br>";
-echo "Hello PHP! redirected" . $space;
-echo $_SERVER["DOCUMENT_ROOT"] . $space;
-echo $_SERVER["HTTP_HOST"] . $space;
-?>
-<img src="img/layout/crush-test-dummies.png">
+<h1>Hello world!</h1>
 </body>
 </html>
