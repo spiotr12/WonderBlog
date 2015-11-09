cda6f2a4eb4f452af61b8404a266a920f6981891
spiotr12 <starzec.piotr.12@gmail.com>
2015-11-09 16:53:17 +0000

    Add menu

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -8,13 +8,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">WonderBlog</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
             </ul>
         </div>
         <!--/.nav-collapse -->
