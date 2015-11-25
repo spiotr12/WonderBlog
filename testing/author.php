8feeb00ba32629e493abf3d20b17e3982b694b20
1303499 <ruairigray@gmail.com>
2015-11-25 14:19:40 +0000

    q

--- testing/author.php
+++ testing/author.php
@@ -40,7 +40,7 @@ renderHeader("WonderBlog!", $meta, $css, $js);
                 </div>
                 <div class="col-md-8">
                     <h2>Heading</h2>
-                    <p>Donec id elit non  mi porta gravid a at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p>Donec id elit non  mi porta gravid a at ege t metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
                     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
                 </div>
             </div>
