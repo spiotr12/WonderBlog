d0b016bb14b5dc4df7f9201255e5852ac1eb650d
1303788 <lloydjack1@hotmail.co.uk>
2015-11-25 14:21:30 +0000

    Yo

--- testing/login.php
+++ testing/login.php
@@ -15,7 +15,7 @@ $css = array(
 //    "css/bootstrap-theme.min.css",
     "css/theme.min.css",
     "css/main.css",
-    //"css/login.css",
+    "css/login.css",
 
 
 );
@@ -34,118 +34,78 @@ renderHeader("WonderBlog!", $meta, $css, $js);
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-6 col-md-7 col-sm-9">
-
-            <h4 style="border-bottom: 1px solid #c5c5c5;">
-                <i class="glyphicon glyphicon-user">
-                </i>
-                Account Access
-            </h4>
-            <div style="padding: 20px;" id="form-olvidado">
-                <form accept-charset="UTF-8" role="form" id="login-form" method="post">
-                    <h4 class="">
-                        Signin!
-                    </h4>
-                    <fieldset>
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            @
-          </span>
-                            <input class="form-control" placeholder="Email" name="email" type="email" required="" autofocus="">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <a href="#" class="active" id="login-form-link">Login</a>
                         </div>
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            <i class="glyphicon glyphicon-lock">
-            </i>
-          </span>
-                            <input class="form-control" placeholder="Password" name="password" type="password" value="" required="">
+                        <div class="col-xs-6">
+                            <a href="#" id="register-form-link">Register</a>
                         </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-block">
-                                Access
-                            </button>
-                            <p class="help-block">
-                                <a class="pull-right text-muted" href="#" id="olvidado"><small>Forgot your password?</small></a>
-                                <a class="pull-left text-muted" href="#" id="olvidado2"><small>Signup!</small></a>
-
-                            </p>
+                    </div>
+                    <hr>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group text-center">
+                                    <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+                                    <label for="remember"> Remember Me</label>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-sm-6 col-sm-offset-3">
+                                            <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="text-center">
+                                                <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: none;">
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-sm-6 col-sm-offset-3">
+                                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </fieldset>
-                </form>
-            </div>
-
-            <div style="display: none;" id="form-olvidado1">
-                <h4 class="">
-                    Forgot your password?
-                </h4>
-                <form accept-charset="UTF-8" role="form" id="login-recordar" method="post">
-                    <fieldset>
-        <span class="help-block">
-          Email address you use to log in to your account
-          <br>
-          We'll send you an email with instructions to choose a new password.
-        </span>
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            @
-          </span>
-                            <input class="form-control" placeholder="Email" name="email" type="email" required="">
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block" id="btn-olvidado">
-                            Continue
-                        </button>
-                        <p class="help-block">
-                            <a class="text-muted" href="#" id="acceso1"><small>Account Access</small></a>
-
-                        </p>
-                    </fieldset>
-                </form>
-            </div>
-            <div style="display: none;" id="form-olvidado2">
-                <h4 class="">
-                    Welcome!
-                </h4>
-                <form accept-charset="UTF-8" role="form" id="login-recordar" method="post">
-                    <fieldset>
-        <span class="help-block">
-          Signup page
-          <br>
-          Please sign up and start!
-        </span>
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            @
-          </span>
-                            <input class="form-control" placeholder="Email" name="email" type="email" required="">
-                        </div>
-
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            <i class="glyphicon glyphicon-lock">
-            </i>
-          </span>
-                            <input class="form-control" placeholder="Password" name="password_new" type="password" required="">
-                        </div>
-                        <div class="form-group input-group">
-          <span class="input-group-addon">
-            <i class="glyphicon glyphicon-lock">
-            </i>
-          </span>
-                            <input class="form-control" placeholder="Repeat Password" name="password_new_2" type="password" required="">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary btn-block" id="btn-olvidado">
-                            Continue
-                        </button>
-                        <p class="help-block">
-                            <a class="text-muted" href="#" id="acceso2"><small>Account Access</small></a>
-                        </p>
-                    </fieldset>
-                </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>>
+</div>
 
 </body>
 </html>
