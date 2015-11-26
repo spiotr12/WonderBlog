c1e0eaa99add93796c897f9f09ed88631341ffd6
1310903 <ryanj1992@hotmail.co.uk>
2015-11-26 09:43:32 +0000

    jumbotron

--- testing/index.php
+++ testing/index.php
@@ -38,11 +38,6 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
             <div id="custom-search-input">
                 <div class="input-group col-md-12">
                     <input type="text" class="  search-query form-control" placeholder="Search" />
-                        <span class="input-group-btn">
-                            <button class="btn btn-danger" type="button">
-                                <span class=" glyphicon glyphicon-search"></span>
-                            </button>
-                                </span>
                 </div>
             </div>
         </div>
