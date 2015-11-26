9d74017296c6093981ec6f08b105293011399f19
1310903 <ryanj1992@hotmail.co.uk>
2015-11-26 11:26:37 +0000

    jumbotron

--- testing/index.php
+++ testing/index.php
@@ -41,7 +41,7 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
                 </div>
             </div>
         </div>
-        <div class = "row">
+        <div id = "buttonGroup" class = "row">
             <div id = "searchAuthor" class = "col-md-6">
                 <button type="button" class="btn btn-danger">Search Author</button>
             </div>
