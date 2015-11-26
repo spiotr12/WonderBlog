7c3615c10908aeff2d7492c2ea7ce1cf6c3b2959
1310903 <ryanj1992@hotmail.co.uk>
2015-11-26 10:53:02 +0000

    jumbotron

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -13,7 +13,7 @@
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="">Home</a></li>
-                <li class=""><a href="#">http://wonderblog.azurewebsites.net/testing/login.php</a></li>
+                <li class=""><a href="http://wonderblog.azurewebsites.net/testing/login.php">Login</a></li>
             </ul>
         </div>
         <!--/.nav-collapse -->
