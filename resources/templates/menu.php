ff4a3b47f4d4d4e694a8ff0c3812964cf0253b9b
1303788 <lloydjack1@hotmail.co.uk>
2015-12-01 12:13:11 +0000

    yo

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -15,7 +15,7 @@
                 <li class=""><a href="http://wonderblog.azurewebsites.net/testing">Home</a></li>
                 <li class=""><a href="http://wonderblog.azurewebsites.net/testing/login.php">Login</a></li>
             </ul>
-            <div class="col-sm-3 col-md-3 pull-right">
+            <div class="col-sm-6 col-md-6 pull-right">
                 <form class="navbar-form" role="search">
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
