b82e80683b41ca60a67d25ecd508506317786837
spiotr12 <starzec.piotr.12@gmail.com>
2015-12-03 13:20:46 +0000

    Testing login

--- testing/index.php
+++ testing/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once("../resources/config.php");
 require_once(LIBRARY_PATH . "/templating_functions.php");
+require_once("./php/db_connect.php");
+
+session_name("sec_sess_wonderblog");
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,31 +32,40 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
 ?>
 <body>
 
-<?php require_once("../resources/templates/menu.php"); ?>
+<?php
+require_once("../resources/templates/menu.php");
 
-<div class = "jumbotron">
-    <div class = "container">
+if(login_check($mysqli)){
+    echo "<h1>logged in</h1>";
+}
+
+?>
+
+<div class="jumbotron">
+    <div class="container">
         <h1>WanderBlog</h1>
+
         <p>The place to upload and explore adventures!</p>
+
         <div class="row">
             <div id="mainSearch">
                 <div class="input-group col-md-8 col-md-offset-2">
-                    <input type="text" class="search-query form-control" placeholder="Search for author or adventures" />
+                    <input type="text" class="search-query form-control" placeholder="Search for author or adventures"/>
                 </div>
             </div>
         </div>
-        <div id = "buttonGroup" class = "row">
-            <div id = "searchAuthor" class = "col-md-6">
+        <div id="buttonGroup" class="row">
+            <div id="searchAuthor" class="col-md-6">
                 <button type="button" class="btn btn-danger">Search Author</button>
             </div>
-            <div id = "searchAdventure" class = "col-md-6">
+            <div id="searchAdventure" class="col-md-6">
                 <button type="button" class="btn btn-danger">Search Adventure</button>
             </div>
         </div>
     </div>
 </div>
 
-<div id = "topAdventure" class = "container">
+<div id="topAdventure" class="container">
     <div class="row">
         <div class="col-md-12">
             <h1>Top 5 Adventures</h1>
@@ -60,13 +73,16 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
     </div>
 </div>
 
-<div id = "top1" class = "container">
+<div id="top1" class="container">
     <div class="row">
         <div class="col-md-4">
-            <img src="http://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349132/big_thumb_f3d6cfe01fbc551c76dce58d36d9f090.jpg" class="img-rounded" alt="Cinque Terre" width = "304px" height = "228px">
+            <img src="http://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349132/big_thumb_f3d6cfe01fbc551c76dce58d36d9f090.jpg" class="img-rounded" alt="Cinque Terre"
+                 width="304px" height="228px">
         </div>
         <div class="col-md-8">
-            <p>Donec id elit non mi porta gravidda at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>Donec id elit non mi porta gravidda at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
             <div class="rating-select">
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
@@ -74,37 +90,21 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
             </div>
-                <a class="btn btn-info" href="#" role="button">View details &raquo;</a>
+            <a class="btn btn-info" href="#" role="button">View details &raquo;</a>
         </div>
     </div>
 </div>
 
-<div id = "top2" class = "container">
+<div id="top2" class="container">
     <div class="row">
         <div class="col-md-4">
-            <img src="http://topwalls.net/wallpapers/2012/01/Nature-sea-scenery-travel-photography-image-800x1280.jpg" class="img-rounded" alt="Cinque Terre" width = "304px" height = "228px">
+            <img src="http://topwalls.net/wallpapers/2012/01/Nature-sea-scenery-travel-photography-image-800x1280.jpg" class="img-rounded" alt="Cinque Terre" width="304px"
+                 height="228px">
         </div>
         <div class="col-md-8">
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <div class="rating-select">
-                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
-                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
-                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
-                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
-                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
-            </div>
-            <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
-        </div>
-    </div>
-</div>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 
-<div id = "top1" class = "container">
-    <div class="row">
-        <div class="col-md-4">
-            <img src="http://77wallpapers.com/wp-content/uploads/2014/10/tropical-waterfall-scenery-wide.jpg" class="img-rounded" alt="Cinque Terre" width = "304px" height = "228px">
-        </div>
-        <div class="col-md-8">
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <div class="rating-select">
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
@@ -117,13 +117,15 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
     </div>
 </div>
 
-<div id = "top2" class = "container">
+<div id="top1" class="container">
     <div class="row">
         <div class="col-md-4">
-            <img src="http://upload.wikimedia.org/wikipedia/commons/6/68/Soufriere_Hills.jpg" class="img-rounded" alt="Cinque Terre" width = "304px" height = "228px">
+            <img src="http://77wallpapers.com/wp-content/uploads/2014/10/tropical-waterfall-scenery-wide.jpg" class="img-rounded" alt="Cinque Terre" width="304px" height="228px">
         </div>
         <div class="col-md-8">
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
             <div class="rating-select">
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
@@ -136,13 +138,37 @@ renderHeader("WonderBlog! [testing]", $meta, $css, $js);
     </div>
 </div>
 
-<div id = "top3" class = "container">
+<div id="top2" class="container">
     <div class="row">
         <div class="col-md-4">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mt._Everest_from_Gokyo_Ri_November_5,_2012_Cropped.jpg" class="img-rounded" alt="Cinque Terre" width = "304px" height = "228px">
+            <img src="http://upload.wikimedia.org/wikipedia/commons/6/68/Soufriere_Hills.jpg" class="img-rounded" alt="Cinque Terre" width="304px" height="228px">
         </div>
         <div class="col-md-8">
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+            <div class="rating-select">
+                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
+                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
+                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
+                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
+                <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
+            </div>
+            <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+        </div>
+    </div>
+</div>
+
+<div id="top3" class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mt._Everest_from_Gokyo_Ri_November_5,_2012_Cropped.jpg" class="img-rounded" alt="Cinque Terre"
+                 width="304px" height="228px">
+        </div>
+        <div class="col-md-8">
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
             <div class="rating-select">
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
                 <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span></div>
