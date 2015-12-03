71b17fe37422feb20860f65ddf4ff025351589cc
1303788 <lloydjack1@hotmail.co.uk>
2015-12-03 13:19:55 +0000

    yo

--- testing/adventure.php
+++ testing/adventure.php
@@ -42,125 +42,134 @@ $id = $_GET["id"];
 // execute the SQL query
 //$description = $mysqli->query($sql_query);
 
-$stmt = new mysqli_stmt($mysqli, "SELECT description FROM adventures WHERE id = $id");
+$stmt = new mysqli_stmt($mysqli, "SELECT description FROM adventures WHERE id = ?");
 
 if ($stmt) {
     $stmt->bind_param("i", $id);
     if ($stmt->execute()) {
         $stmt->bind_result($description);
         $stmt->store_result();
+        if ($stmt->num_rows() == 1) {
+            while ($stmt->fetch()) {
 
 
-        ?>
+                ?>
 
 
-        <div class="container">
-            <div class="row">
-                <h1 class="text-center">Adventure Title</h1>
-            </div>
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <img class="img-responsive" src="http://placehold.it/1200x440/cccccc/ffffff">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-5 col-md-offset-1">
-                    <h2>Description</h2>
+                <div class="container">
+                    <div class="row">
+                        <h1 class="text-center">Adventure Title</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <img class="img-responsive" src="http://placehold.it/1200x440/cccccc/ffffff">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5 col-md-offset-1">
+                            <h2>Description</h2>
 
-                    <?php echo $description; ?>
-                </div>
-                <div class="col-md-3 col-md-offset-2 text-center">
-                    <h2>Rating</h2>
-                    <span class="glyphicon glyphicon-star"></span>
-                    <span class="glyphicon glyphicon-star"></span>
-                    <span class="glyphicon glyphicon-star"></span>
-                    <span class="glyphicon glyphicon-star"></span>
-                    <span class="glyphicon glyphicon-star"></span>
+                            <?php echo $description; ?>
+                        </div>
+                        <div class="col-md-3 col-md-offset-2 text-center">
+                            <h2>Rating</h2>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
 
-                    <h2>Tags</h2>
-                    <ul class="list-unstyled">
-                        <li>
-                            #iLoveTags
-                        </li>
-                        <li>
-                            #mayTheForceBeWithYou
-                        </li>
-                        <li>
-                            #imGoingOnAnAdventure
-                        </li>
-                        <li>
-                            #youShallNotPass
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-5 col-md-offset-1 comments-section">
-                    <h2>Comments</h2>
-                    <?php for ($i = 0; $i < 4; $i++): ?>
-                        <section>
-                            <div class="">
-                                <label class="">Piotrek</label>
-                                <label class="pull-right">2015-12-01</label>
-                            </div>
+                            <h2>Tags</h2>
+                            <ul class="list-unstyled">
+                                <li>
+                                    #iLoveTags
+                                </li>
+                                <li>
+                                    #mayTheForceBeWithYou
+                                </li>
+                                <li>
+                                    #imGoingOnAnAdventure
+                                </li>
+                                <li>
+                                    #youShallNotPass
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5 col-md-offset-1 comments-section">
+                            <h2>Comments</h2>
+                            <?php for ($i = 0; $i < 4; $i++): ?>
+                                <section>
+                                    <div class="">
+                                        <label class="">Piotrek</label>
+                                        <label class="pull-right">2015-12-01</label>
+                                    </div>
 
-                            <div class="comment">ahfkjbfkjbasfkjbewak bkjfb ksbbf labfeab jehbf aljbfhbfksbf sk djbf
-                                dksjgoewrihg
-                                dkjsng d
-                            </div>
-                        </section>
-                    <?php endfor; ?>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="carousel slide article-slide" id="adventureCarousel">
-                        <div class="carousel-inner cont-slider">
-                            <?php $carouselRuns = 3 ?>
-                            <?php for ($i = 0; $i < $carouselRuns; $i++): ?>
-                                <div class="item <?php if ($i == 0) echo "active"; ?>">
-                                    <img src="http://placehold.it/1200x440/cccccc/ffffff">
-                                </div>
-                                <div class="item">
-                                    <img src="http://placehold.it/1200x440/999999/cccccc">
-                                </div>
+                                    <div class="comment">ahfkjbfkjbasfkjbewak bkjfb ksbbf labfeab jehbf aljbfhbfksbf sk
+                                        djbf
+                                        dksjgoewrihg
+                                        dkjsng d
+                                    </div>
+                                </section>
                             <?php endfor; ?>
                         </div>
-
-                        <!-- Controls -->
-                        <a class="left carousel-control" href="#adventureCarousel" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                        </a>
-                        <a class="right carousel-control" href="#adventureCarousel" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                        </a>
-
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators visible-lg visible-md">
-                            <?php for ($i = 0; $i < $carouselRuns; $i++): ?>
-                                <li class="<?php if ($i == 0) echo "active"; ?>" data-slide-to="<?php echo 2 * $i; ?>"
-                                    data-target="#adventureCarousel">
-                                    <img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
-                                </li>
-                                <li class="" data-slide-to="<?php echo 2 * $i + 1; ?>" data-target="#adventureCarousel">
-                                    <img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
-                                </li>
-                            <?php endfor; ?>
-                        </ol>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <div class="carousel slide article-slide" id="adventureCarousel">
+                                <div class="carousel-inner cont-slider">
+                                    <?php $carouselRuns = 3 ?>
+                                    <?php for ($i = 0; $i < $carouselRuns; $i++): ?>
+                                        <div class="item <?php if ($i == 0) echo "active"; ?>">
+                                            <img src="http://placehold.it/1200x440/cccccc/ffffff">
+                                        </div>
+                                        <div class="item">
+                                            <img src="http://placehold.it/1200x440/999999/cccccc">
+                                        </div>
+                                    <?php endfor; ?>
+                                </div>
 
-        <script type="text/javascript">
-            $('#adventureCarousel').carousel({
-                interval: 4000
-            });
-        </script>
+                                <!-- Controls -->
+                                <a class="left carousel-control" href="#adventureCarousel" role="button"
+                                   data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#adventureCarousel" role="button"
+                                   data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
 
-    <?php
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators visible-lg visible-md">
+                                    <?php for ($i = 0; $i < $carouselRuns; $i++): ?>
+                                        <li class="<?php if ($i == 0) echo "active"; ?>"
+                                            data-slide-to="<?php echo 2 * $i; ?>"
+                                            data-target="#adventureCarousel">
+                                            <img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
+                                        </li>
+                                        <li class="" data-slide-to="<?php echo 2 * $i + 1; ?>"
+                                            data-target="#adventureCarousel">
+                                            <img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
+                                        </li>
+                                    <?php endfor; ?>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <script type="text/javascript">
+                    $('#adventureCarousel').carousel({
+                        interval: 4000
+                    });
+                </script>
+
+                <?php
+            }
+        }
     }
 }
 ?>
