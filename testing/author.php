454ef282ef46d4953209d6e8d3da1c125c2338ad
1303499 <ruairigray@gmail.com>
2015-12-07 14:33:20 +0000

    q

--- testing/author.php
+++ testing/author.php
@@ -163,9 +163,7 @@ foreach ($adventure as $stone) {
                 class="img-rounded" alt="Cinque Terre" width="250" height="228px">
         </div>
         <div class="col-md-9">
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                euismod. Donec sed odio dui. </p>
+            <p> <?php echo $ad_description ?></p>
 
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
