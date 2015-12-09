23e52d0fc2debc03e6d94107f58cb4237c95ce3c
1303499 <ruairigray@gmail.com>
2015-12-09 13:21:59 +0000

    q

--- testing/author.php
+++ testing/author.php
@@ -98,7 +98,7 @@ if ($stmt1->num_rows() == 1) {
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">Edit Info</h4>
+                                        <h6 class="modal-title">Edit Info</h6>
                                     </div>
                                     <div class="modal-body">
                                         <div class="form-group">
