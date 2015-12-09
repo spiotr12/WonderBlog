874072cb87987a8da5d99999408ea82f43a8f49b
Piotrek <starzec.piotr.12@gmail.com>
2015-12-09 20:43:31 +0000

    Working on admin panel: get json of users

--- testing/js/admin.js
+++ testing/js/admin.js
@@ -10,11 +10,13 @@ function getUsers() {
     var tbody = $("#usersTable").children("tbody");
     $.getJSON("./php/data_users.php", function (result) {
         $.each(result, function (i, user) {
+            console.log(user);
             tbody.append("<tr>");
             $.each(user, function (j, userData) {
                 tbody.append("<td>");
                 tbody.append(userData);
                 tbody.append("</td>");
+                console.log(userData);
             });
             tbody.append("</tr>");
         });
