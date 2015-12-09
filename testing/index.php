4216a52743d1145b03fb5adf863f707924d50a83
1310903 <ryanj1992@hotmail.co.uk>
2015-12-09 12:45:35 +0000

    Merge branch 'master' of H:\YEAR 3 BIT\Web Development\WonderBlog2 with conflicts.

--- testing/index.php
+++ testing/index.php
@@ -2,13 +2,10 @@
 require_once("../resources/config.php");
 require_once(LIBRARY_PATH . "/templating_functions.php");
 require_once(LIBRARY_PATH . "/functions.php");
-
-// TO ALLOW USERS TO LOGIN ON EACH PAGE PLEASE COPY THIS CODE
 require_once("./php/db_connect.php");
-require_once("./php/classes/Login.class.php");
-$login = new Login();
-// END OF LOGIN SCRIPT
 
+session_name("sec_sess_rewdt");
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,20 +37,33 @@ renderHeader("WonderBlog! [testing2]", $meta, $css, $js);
 require_once("../resources/templates/menu.php");
 
 
+$teste =  "<h1>logged in?:";
+if(login_check($mysqli)){
+    $teste .= "TRUE";
+} else {
+    $teste .= "FALSE";
+}
+$teste .= "</h1>";
+echo $teste;
+
+
 //Gather adventures
 $adventures = array();
 $total_progress = 0;
 
-$stmt = new mysqli_stmt($mysqli, "SELECT id, country, description FROM adventures WHERE id = ?");
+$stmt = new mysqli_stmt($mysqli, "SELECT id, country, city, google_location, description, date FROM adventures WHERE id = ? ORDER BY date");
 if ($stmt) {
     $stmt->bind_param("i", $adventures['id']);
     if ($stmt->execute()) {
-        $stmt->bind_result($id, $country, $description);
+        $stmt->bind_result($id, $country, $city,  $google_location, $description, $date);
         while ($stmt->fetch()) {
             $temp_arr = array(
                 'id' => $id,
                 'country' => $country,
+                'city' => $city,
+                'google_location' => $google_location,
                 'description' => $description,
+                'date' => date("d/m/y", strtotime($date)),
             );
             array_push($adventures, $temp_arr);
         }
@@ -61,63 +71,193 @@ if ($stmt) {
 } else {
     echo "<h3>" . mysqli_error($mysqli) . "</h3>";
 }
-
 ?>
 
-    <div class="jumbotron">
-        <div class="container">
-            <h1>WanderBlog</h1>
+        <?php
+//Print adventure
+        $ms_total = $total_progress;
+        foreach ($adventures as $stone) {
+            $ms_total -= $stone['progress'];
+            ?>
+            <div class="jumbotron">
+                <div class="container">
+                    <h1>WanderBlog</h1>
 
-            <p>The place to upload and explore adventures!</p>
+                    <p>The place to upload and explore adventures!</p>
 
-            <div class="row">
-                <div id="mainSearch">
-                    <div class="input-group col-md-8 col-md-offset-2">
-                        <input type="text" class="search-query form-control"
-                               placeholder="Search for author or adventures"/>
+                    <div class="row">
+                        <div id="mainSearch">
+                            <div class="input-group col-md-8 col-md-offset-2">
+                                <input type="text" class="search-query form-control"
+                                       placeholder="Search for author or adventures"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="buttonGroup" class="row">
+                        <div id="searchAuthor" class="col-md-6">
+                            <button type="button" class="btn btn-danger">Search Author</button>
+                        </div>
+                        <div id="searchAdventure" class="col-md-6">
+                            <button type="button" class="btn btn-danger">Search Adventure</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div id="buttonGroup" class="row">
-                <div id="searchAuthor" class="col-md-6">
-                    <button type="button" class="btn btn-danger">Search Author</button>
-                </div>
-                <div id="searchAdventure" class="col-md-6">
-                    <button type="button" class="btn btn-danger">Search Adventure</button>
+
+            <div id="topAdventure" class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Top 5 Adventures</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div id="topAdventure" class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Top 5 Adventures</h1>
+            <div id="top1" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img
+                            src="http://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349132/big_thumb_f3d6cfe01fbc551c76dce58d36d9f090.jpg"
+                            class="img-rounded" alt="Cinque Terre"
+                            width="304px" height="228px">
+                    </div>
+                    <div class="col-md-8">
+                        <h2><?php echo $stone['description']; ?></h2>
+
+                        <div class="rating-select">
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                        </div>
+                        <a class="btn btn-info" href="#" role="button">View details &raquo;</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
-<?php
-foreach ($adventures as $stone) {
-?>
+            <div id="top2" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img
+                            src="http://topwalls.net/wallpapers/2012/01/Nature-sea-scenery-travel-photography-image-800x1280.jpg"
+                            class="img-rounded" alt="Cinque Terre" width="304px"
+                            height="228px">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                            Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 
-<div id="top1" class="container">
-    <div class="row">
-        <div class="col-md-3">
-            <img
-                src="http://www.wallpaperup.com/uploads/wallpapers/2014/05/04/349132/big_thumb_f3d6cfe01fbc551c76dce58d36d9f090.jpg"
-                class="img-rounded" alt="Cinque Terre" width="250" height="228px">
-        </div>
-        <div class="col-md-9">
-            <p> <?php echo $stone['description'] ?></p>
+                        <div class="rating-select">
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                        </div>
+                        <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+                    </div>
+                </div>
+            </div>
 
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-    </div>
-</div>
+            <div id="top1" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img
+                            src="http://77wallpapers.com/wp-content/uploads/2014/10/tropical-waterfall-scenery-wide.jpg"
+                            class="img-rounded" alt="Cinque Terre" width="304px" height="228px">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                            Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 
-<?php
-}
-?>
-</body>
-</html>
+                        <div class="rating-select">
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                        </div>
+                        <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="top2" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="http://upload.wikimedia.org/wikipedia/commons/6/68/Soufriere_Hills.jpg"
+                             class="img-rounded" alt="Cinque Terre" width="304px" height="228px">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                            Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+                        <div class="rating-select">
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                        </div>
+                        <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="top3" class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mt._Everest_from_Gokyo_Ri_November_5,_2012_Cropped.jpg"
+                            class="img-rounded" alt="Cinque Terre"
+                            width="304px" height="228px">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                            Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+                        <div class="rating-select">
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <div class="btn btn-default btn-sm"><span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                        </div>
+                        <p><a class="btn btn-info" href="#" role="button">View details &raquo;</a></p>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+            ?>
+            </body>
+            </html>
