ef998a976174220a60c115e1b6691f10fca7f6cd
Piotrek <starzec.piotr.12@gmail.com>
2015-12-09 19:24:11 +0000

    Working on admin panel: get json of users

--- testing/php/data_users.php
+++ testing/php/data_users.php
@@ -5,7 +5,8 @@
  * Date: 2015-12-09
  * Time: 18:16
  */
-require_once("../../resources/config.php");
+require_once(realpath(dirname(__FILE__) . "../../resources/config.php"));
+//require_once("../../resources/config.php");
 require_once("./db_connect.php");
 
 $search = NULL;
