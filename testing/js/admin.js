35e4e4ad6febdfe67877dc45d58aae7fa520a854
Piotrek <starzec.piotr.12@gmail.com>
2015-12-10 13:27:17 +0000

    Working on admin panel: get json of users

--- testing/js/admin.js
+++ testing/js/admin.js
@@ -20,10 +20,16 @@ function getUsers() {
         $.each(result, function (i, user) {
             //console.log(user);
             var row = "<tr>";
-            row += '<tr><a href="./author.php?id=' + user.id + '">' + user.fname + '</a></tr>';
-            row += '<tr><a href="./author.php?id=' + user.id  + '">' + user.lname + '</a></tr>';
+            //row += '<tr><a href="./author.php?id=' + user.id + '">' + user.fname + '</a></tr>';
+            //row += '<tr><a href="./author.php?id=' + user.id  + '">' + user.lname + '</a></tr>';
+            row += '<tr>' + user['fname'] + '</tr>';
+            console.log(user['fname']);
+            row += '<tr>' + user['lname'] + '</tr>';
+            console.log(user['lname']);
             row += '<tr>' + user['privilege'] + '</tr>';
+            console.log(user['privilege']);
             row += '<tr>' + user['verified'] + '</tr>';
+            console.log(user['verified']);
             row += '</tr>';
             tbody.append(row);
         });
