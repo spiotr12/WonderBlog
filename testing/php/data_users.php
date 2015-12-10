4b05275c09e088be5c0f85d697b01c965ee4a9f5
Piotrek <starzec.piotr.12@gmail.com>
2015-12-10 13:40:01 +0000

    Working on admin panel: get json of users

--- testing/php/data_users.php
+++ testing/php/data_users.php
@@ -23,7 +23,7 @@ if ($stmt = $mysqli->prepare("SELECT id, first_name, last_name, privilege, verif
     $stmt->bind_result($id, $fname, $lname, $priv, $veri);
     while($stmt->fetch()){
         $results[$id] = array(
-            'id' => $id,
+            'ix' => $id,
             'fname' => $fname,
             'lname' => $lname,
             'privilege' => $priv,
