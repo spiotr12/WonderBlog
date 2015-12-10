6747047cc448f784ac56794b16e6c31eeb4a1b68
Piotrek <starzec.piotr.12@gmail.com>
2015-12-10 13:20:54 +0000

    Working on admin panel: get json of users

--- testing/js/admin.js
+++ testing/js/admin.js
@@ -20,8 +20,8 @@ function getUsers() {
         $.each(result, function (i, user) {
             //console.log(user);
             var row = "<tr>";
-            row += '<tr><a href="./author.php?id=' + user['id'] + '">' + user['fname'] + '</a></tr>';
-            row += '<tr><a href="./author.php?id=' + user['id'] + '">' + user['lname'] + '</a></tr>';
+            row += '<tr><a href="./author.php?id=' + user.id + '">' + user.fname + '</a></tr>';
+            row += '<tr><a href="./author.php?id=' + user.id  + '">' + user.lname + '</a></tr>';
             row += '<tr>' + user['privilege'] + '</tr>';
             row += '<tr>' + user['verified'] + '</tr>';
             row += '</tr>';
