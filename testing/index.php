9369a388df05d3dd8041fee0deccd79cd653e45a
ryanj1992 <1310903@rgu.ac.uk>
2015-12-14 19:37:29 +0000

    updates

--- testing/index.php
+++ testing/index.php
@@ -41,10 +41,10 @@ renderHeader("WonderBlog! [testing2]", $meta, $css, $js);
 $adventure = array();
 $total_progress = 0;
 // adventure
-$stmtAdventure = new mysqli_stmt($mysqli, "SELECT a.id, a.description, v.adv_id, v.vote, COUNT(vote) as rate FROM adventures A, votes V WHERE A.id = v.adv_id GROUP BY A.id");
+$stmtAdventure = new mysqli_stmt($mysqli, "SELECT a.id, a.description, v.adv_id, v.vote, p.file_ext, p.is_cover, p.id, COUNT(vote) as rate FROM adventures A, votes V, photos P WHERE A.id = v.adv_id GROUP BY A.id");
 if ($stmtAdventure) {
     if ($stmtAdventure->execute()) {
-        $stmtAdventure->bind_result($adventureID, $adventureDesc, $voteAdvID, $vote, $voteCount);
+        $stmtAdventure->bind_result($adventureID, $adventureDesc, $voteAdvID, $vote, $photoExt, $photoCover, $voteCount);
         while ($stmtAdventure->fetch()) {
             $temp_arr = array(
                 'adventureID' => $adventureID,
@@ -52,6 +52,8 @@ if ($stmtAdventure) {
                 'voteAdvID' => $voteAdvID,
                 'vote' => $vote,
                 'voteCount' => $voteCount,
+                'photoExt' => $photoExt,
+                'cover' => $photoCover,
                 //'progress' => $ad_progress
             );
             array_push($adventure, $temp_arr);
