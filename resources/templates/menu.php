7e7b14cc099ef7ccf3e78b768b1c07e72a4ac415
Piotr <starzec.piotr.12@gmail.com>
2015-12-14 11:41:25 +0000

    Search

--- resources/templates/menu.php
+++ resources/templates/menu.php
@@ -38,10 +38,10 @@ if (strlen($_SERVER['QUERY_STRING'])) {
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Search" name="q" id="srch-term">
                         <div class="input-group-btn">
-                            <button class="btn btn-default" type="submit" name="author"><span
+                            <button class="btn btn-default" type="submit" name="search_type" value="authors"><span
                                     class="glyphicon glyphicon-search"> Author</span>
                             </button>
-                            <button class="btn btn-default" type="submit" name="adventure"><span
+                            <button class="btn btn-default" type="submit" name="search_type" value="adventures"><span
                                     class="glyphicon glyphicon-search"> Adventure</span>
                             </button>
                         </div>
