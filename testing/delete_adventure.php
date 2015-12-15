535959fdeeaab6c86f5580dd58ca98df78e04255
ruairig <ruairigray@gmail.com>
2015-12-15 22:13:07 +0000

    Edit adventure

--- testing/delete_adventure.php
+++ testing/delete_adventure.php
@@ -4,4 +4,7 @@ require_once("../resources/config.php");
 require_once("./php/db_connect.php");
 $adventureID = $_POST["test"];
 echo $adventureID;
+
+$delete = new mysqli_stmt ($mysqli, "DELETE FROM adventures WHERE id = '$adventureID'");
 ?>
+
