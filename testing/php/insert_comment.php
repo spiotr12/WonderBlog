0404502e100657d7d2db988947a8004dc8bc6d37
1303788 <lloydjack1@hotmail.co.uk>
2015-12-15 14:49:55 +0000

    newy

--- testing/php/insert_comment.php
+++ testing/php/insert_comment.php
@@ -8,11 +8,6 @@ $id = $_GET["id"];
 
 $sql= "INSERT INTO `comments` (`id`, `user_id`, `comment`, `date`, `adv_id`) VALUES ('NULL', '1', '$_POST[comment]', CURRENT_TIMESTAMP , $id)";
 
-if (!mysqli_query($con,$sql))
-{
-    die('Error: ' . mysqli_error($con));
-}
-echo "1 comment added";
 
-mysqli_close($con);
+$mysqli->close();
 ?>
