54bd214040169e97381319b6a9b30c68977ed904
ruairig <ruairigray@gmail.com>
2015-12-15 22:20:32 +0000

    Edit adventure

--- testing/delete_adventure.php
+++ testing/delete_adventure.php
@@ -2,9 +2,19 @@
 
 require_once("../resources/config.php");
 require_once("./php/db_connect.php");
+
 $adventureID = $_POST["test"];
 echo $adventureID;
 
 $delete = new mysqli_stmt ($mysqli, "DELETE FROM adventures WHERE id = $adventureID");
+
+if ($mysqli->query($delete) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $mysqli->error;
+}
+
+$mysqli->close();
+
 ?>
 
