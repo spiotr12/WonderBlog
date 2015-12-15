c9cbb88def1d5d5739ef08bdf842a76bfa5f3826
Piotr <starzec.piotr.12@gmail.com>
2015-12-15 17:01:21 +0000

    Registration

--- testing/php/classes/Registration.class.php
+++ testing/php/classes/Registration.class.php
@@ -59,10 +59,10 @@ class Registration {
                     $this->errors[] = "Sorry, that username / email address is already taken.";
                 } else {
                     // write new user's data into database
-                    $stmt2 = new mysqli_stmt($this->db_connection, "INSERT INTO users (first_name, last_name, password, email) VALUES (?, ?, ?, ?)");
-                    $stmt2->bind_param("ssss", $user_first_name, $user_last_name, $user_password_hash, $user_email);
+                    $stmt = new mysqli_stmt($this->db_connection, "INSERT INTO users (first_name, last_name, password, email) VALUES (?, ?, ?, ?);");
+                    $stmt->bind_param("ssss", $user_first_name, $user_last_name, $user_password_hash, $user_email);
                     // if user has been added successfully
-                    if ($stmt2->execute()) {
+                    if ($stmt->execute()) {
                         $this->messages[] = "Your account has been created successfully. You can now log in.";
                     } else {
                         $this->errors[] = "Sorry, your registration failed. Please go back and try again.";
