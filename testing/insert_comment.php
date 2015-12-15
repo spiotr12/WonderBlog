2fb39837e1b4986e6e3f36b1be69b491ec718f37
1303788 <lloydjack1@hotmail.co.uk>
2015-12-15 15:58:16 +0000

    newy

--- testing/insert_comment.php
+++ testing/insert_comment.php
@@ -8,13 +8,18 @@ $userId = $_POST['user_id'];
 $comment = $_POST['comment'];
 $date = date("Y-m-d H:i:S");
 
+echo var_dump($_POST);
+
 
 $stmt = new mysqli_stmt($mysqli, "INSERT INTO comments (user_id, adv_id, comment, date) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("iiss",$userId, $advId, $comment, $date);
+if ($stmt) {
+    $stmt->bind_param("iiss", $userId, $advId, $comment, $date);
     $stmt->execute();
+} else {
+    echo "stmt error";
+}
 
-
-$mysqli->close();
+//$mysqli->close();
 
 //header("location: ./adventure.php?id=$advId");
 
