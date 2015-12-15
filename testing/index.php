e5c2580961e3878acfef725b76b023931c2ef69a
1310903 <ryanj1992@hotmail.co.uk>
2015-12-15 17:23:55 +0000

    jumbotron

--- testing/index.php
+++ testing/index.php
@@ -105,7 +105,7 @@ if ($stmtAdventure) {
         <div class="row">
             <div class="col-md-3">
                 <img
-                    src="./img/contents/<?php echo $stone['photoID']; ?>.<?php echo $stone['photoExt']; ?>"
+                    src="./img/contents/<?php echo $photoID; ?>.<?php echo $photoExt; ?>"
                     class="img-rounded" alt="Cinque Terre" width="250" height="228px">
             </div>
             <div class="col-md-9">
