264da8e8d0150ae5ad2410c34bb5dcc43e59b272
1303499 <ruairigray@gmail.com>
2015-12-15 11:59:22 +0000

    create adventure form

--- testing/CreateAdventure.php
+++ testing/CreateAdventure.php
@@ -44,7 +44,7 @@ renderHeader("WonderBlog!", $meta, $css, $js);
     <div class="modal-dialog">
 
         <!-- Modal content-->
-<!--        <div class="modal-content">-->
+        <div class="modal-content">
 <!--            <div class="modal-header">-->
 <!--                <button type="button" class="close" data-dismiss="modal">&times;</button>-->
 <!--                <h4 class="modal-title">Edit Info</h4>-->
@@ -75,7 +75,7 @@ renderHeader("WonderBlog!", $meta, $css, $js);
         </div>
 
     </div>
-<!--</div>-->
+</div>
 
 </body>
 
