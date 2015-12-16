3b9e1a7bcbe7638343bad2b0b7501b375e347e44
1303788 <lloydjack1@hotmail.co.uk>
2015-12-16 16:44:59 +0000

    newy

--- testing/admin_votes.php
+++ testing/admin_votes.php
@@ -5,7 +5,7 @@ require_once("./php/db_connect.php");
 
 $advId = $_POST['adv_id'];
 $adminVote = $_POST['admin_vote'];
-
+echo $adminVote;
 $stmt = new mysqli_stmt($mysqli, "INSERT INTO adventures (admin_vote) VALUES (?) WHERE adv_id = ?");
 if ($stmt) {
     $stmt->bind_param("ii", $adminVote, $advID);
@@ -13,4 +13,4 @@ if ($stmt) {
 
     $mysqli->close();
 
-    header("location: ./adventure.php?id=$advId");
+  //  header("location: ./adventure.php?id=$advId");
