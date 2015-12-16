5e6a6d60a4eb02d82446789cc7c3acb265ca3f10
1303788 <lloydjack1@hotmail.co.uk>
2015-12-16 19:04:28 +0000

    newy

--- testing/adventure.php
+++ testing/adventure.php
@@ -112,7 +112,7 @@ while ($stmt2->fetch()) {
 
             <?php if (privilegeCheck($mysqli, $_SESSION['id']) == 0): ?>
             <form action="admin_votes.php" method=post>
-                Current admin vote: <?php echo $adminVote?><br>
+                Current admin vote: <?php echo $adminVote ?><br>
                 Update admin vote to: <input type="number" name="admin_votes" min="-1000000" max="1000000"/>
                 <input type="hidden" name="adv_id" value="<?php echo $id; ?>">
                 <input type='submit'/>
@@ -196,6 +196,16 @@ while ($stmt2->fetch()) {
                                 ?>
 
                             </div>
+                            <div
+                            <?php if ($row['user_id'] == $_SESSION['id']): ?>
+                            <form action="edit_comment.php" method=post>
+                                <textarea rows="3" cols="80" name='editComment' id='editComment'
+                                              placeholder="<?php echo $row['comment'] ?>"></textarea><br/>
+                                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                <input type='submit'/>
+                            </form>
+
+                                <?php endif; ?>
                         </section>
 
                     </div>
---
+++ testing/edit_comment.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once("../resources/config.php");
+require_once("./php/db_connect.php");
+
+$commentId = $_POST['id'];
+$editedComment = $_POST['editComment'];
+$date = date("Y-m-d H:i:s");
+
+//echo "dump: " . var_dump($_POST) . "<br><br>";
+
+
+$stmt = new mysqli_stmt ($mysqli, "UPDATE comments
+       SET comment = ?, date = ? WHERE id= ?");
+
+if ($stmt) {
+    $stmt->bind_param("ssi", $editedComment, $date, $commentId);
+    $stmt->execute();
+}
+
+
+$mysqli->close();
+
+header("location: ./adventure.php?id=$advId");
+
+?>
