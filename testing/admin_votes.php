c61f17ddb723b7a30bc5ab083733d98a81e72310
1303788 <lloydjack1@hotmail.co.uk>
2015-12-16 17:17:17 +0000

    newy

--- testing/admin_votes.php
+++ testing/admin_votes.php
@@ -5,7 +5,7 @@ require_once("./php/db_connect.php");
 
 $advId = $_POST['adv_id'];
 $adminVote = $_POST['admin_votes'];
-
+echo $adminVote;
 $stmt = new mysqli_stmt($mysqli, "UPDATE adventures SET admin_vote = ? WHERE adv_id = ?");
 if ($stmt) {
     $stmt->bind_param("ii", $adminVote, $advID);
@@ -13,4 +13,4 @@ if ($stmt) {
 
     $mysqli->close();
 
-  header("location: ./adventure.php?id=$advId");
+  //  header("location: ./adventure.php?id=$advId");
