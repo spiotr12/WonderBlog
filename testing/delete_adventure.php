0cbb3dd688b68e8cee34dd760e352c702346cf9c
1303499 <ruairigray@gmail.com>
2015-12-17 12:23:21 +0000

    create adventure form

--- testing/delete_adventure.php
+++ testing/delete_adventure.php
@@ -4,14 +4,14 @@ require_once("../resources/config.php");
 require_once("./php/db_connect.php");
 
 $adventureID = $_POST["test"];
-
+echo $adventureID;
 $stmt = new mysqli_stmt($mysqli, "DELETE FROM adventures WHERE id = ?");
 if ($stmt) {
     $stmt->bind_param("i", $adventureID);
     $stmt->execute();}
 
 
-//echo "DONE";
+
 //$str = 'Location:  ./index '  ;
 //header($str);
 
