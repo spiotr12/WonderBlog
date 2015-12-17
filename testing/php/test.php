a9bfff09fed25685387ecabb74ef7e8a38783fd2
Piotr <starzec.piotr.12@gmail.com>
2015-12-17 15:37:50 +0000

    Search change

--- testing/php/test.php
+++ testing/php/test.php
@@ -6,14 +6,9 @@
  * Time: 12:39
  */
 
-function multiexplode ($delimiters,$string) {
-    $ready = str_replace($delimiters, $delimiters[0], $string);
-    $launch = explode($delimiters[0], $ready);
-    return  $launch;
-}
+require_once("../../resources/config.php");
 
-echo "hello world";
-$str = "elo;Cos;reawfew;23rwefwe,SDFfdwfWE";
-echo "<br>";
-$arr = multiexplode(array(";", ","), $str);
-var_dump($arr);
+
+$dir = "../img/contents/";
+$scan = scandir($dir);
+print_r($scan);
