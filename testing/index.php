1fda7545f96558a260c88bc8c298d108c84b13d0
Piotr <starzec.piotr.12@gmail.com>
2015-12-18 13:44:03 +0000

    Advance search

--- testing/index.php
+++ testing/index.php
@@ -121,8 +121,8 @@ foreach ($adventures as $adv) {
     <div id="top1" class="container">
         <div class="row">
             <div class="col-md-3">
-                <img src="./img/contents/<?php echo $adv['photoID'] . "." . $adv['photoExt']; ?>" class="img-rounded"
-                     alt="Cinque Terre" width="250" height="228px">
+                <img src="./img/contents/<?php echo $adv['photoId'] . "." . $adv['photoExt']; ?>" class="img-rounded"
+                     width="250" height="228px">
             </div>
             <div class="col-md-9">
                 <p> <?php echo $adv['description']; ?></p>
